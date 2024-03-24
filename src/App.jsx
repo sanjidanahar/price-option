27c078bd6d5b5c1534327f40bb162688ddde0c52
sanjidanahar <cse02707453@gmail.com>
2024-03-24 15:29:54 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import DaisyNav from './components/DaisyNav/DaisyNav'
+import './App.css'
+import NavBar from './components/DaisyNav/NavBar/NavBar'
+
+function App() {
+
+
+  return (
+    <>
+
+      <NavBar></NavBar>
+      <DaisyNav></DaisyNav>
+      <h1 className='text-7xl bg-rose-600'>Vite + React</h1>
+
+     
+      
+     
+    </>
+  )
+}
+
+export default App
